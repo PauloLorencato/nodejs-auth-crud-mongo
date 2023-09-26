025270ab1efafed9e9a281a77508306371114ce7
Paulo Victor Lorençato <pvlorencato@gmail.com>
2023-09-26 02:44:39 -0300

    Iniciada lógica básica do servidor

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+  mongoose = require('mongoose'),
+  express = require('express'),
+  auth = require('./routes/auth'),
+  crud = require('./routes/crud'),
+  app = express()
+  DB_USER = process.env.DB_USER
+  DB_PASS = process.env.DB_PASS
+
+// middleware
+app.use(express.json())
+
+// rotas básicas
+app.use('/data', crud)
+app.use('/auth', auth)
+
+// conexão ao banco
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.0o09qhf.mongodb.net/?retryWrites=true&w=majority`)
+  .then(console.log("Conectado ao banco"))
+  .catch((err) => console.log(err))
+
+// escutando uma porta
+app.listen(3000, () => console.log("Rodando na 3000"))
